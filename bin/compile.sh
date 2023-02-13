58c50989ede2cda28077fb888ac722ba427bd113
Victor Felix <victor.felix@qulture.rocks>
2023-02-13 15:31:34 -0300

    rename compile file with initial implementation

--- bin/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -L https://github.com/darold/pgbadger/archive/refs/tags/v12.0.tar.gz
-tar xzf pgbadger-12.0.tar.gz \
-    cd pgbadger-12.0 \
-    perl Makefile.PL \
-    make && sudo make install
